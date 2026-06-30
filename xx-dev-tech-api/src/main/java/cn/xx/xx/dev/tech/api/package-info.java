667f5fed92e53f8335eca98f1fc78d3a8c7d69d5
Xiaoxin0430 <2878647251@qq.com>
2026-06-30 14:31:10 +0800

    feat: 2-init-project

---
+++ xx-dev-tech-api/src/main/java/cn/xx/xx/dev/tech/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author xiaoxin
+ * @description
+ * @create 2026/6/30 14:25
+ */
+
+
+package cn.xx.xx.dev.tech.api;
---
+++ xx-dev-tech-trigger/src/main/java/cn/xx/xx/dev/tech/trigger/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author xiaoxin
+ * @description
+ * @create 2026/6/30 14:26
+ */
+
+
+package cn.xx.xx.dev.tech.trigger;
