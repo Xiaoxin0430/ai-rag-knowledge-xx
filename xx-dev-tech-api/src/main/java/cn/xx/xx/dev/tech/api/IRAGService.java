7ddae7dab8b501700d009ac561826ae4da7d64dd
Xiaoxin0430 <2878647251@qq.com>
2026-07-01 16:55:02 +0800

    feat: 6-rag-api

---
+++ xx-dev-tech-api/src/main/java/cn/xx/xx/dev/tech/api/IRAGService.java
@@ -0,0 +1,14 @@
+package cn.xx.xx.dev.tech.api;
+
+import cn.xx.xx.dev.tech.api.response.Response;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface IRAGService {
+
+    Response<List<String>> queryRagTagList();
+
+    Response<String> uploadFile(String ragTag, List<MultipartFile> files);
+
+}
